ab995e0c11e67b2dd5948d04d8d2faec5cd7a59c
HackerCG <angelos2283@gmail.com>
2023-03-30 13:12:00 +0800

    Site updated: 2023-03-30 13:12:00

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(110.324420,25.278100 , ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
@@ -150,7 +150,7 @@ function showWelcome() {
                     posdesc = "老板来两斤福建人。";
                     break;
                 case "广西壮族自治区":
-                    posdesc = "桂林山水甲天下。";
+                    posdesc = "三月三唱山歌，没你我不看。";
                     break;
                 case "海南省":
                     posdesc = "朝观日出逐白浪，夕看云起收霞光。";
