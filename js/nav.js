f827f6f3d62bc8e51a8f29fb12f770ae85f180ea
HackerCG <angelos2283@gmail.com>
2023-04-13 17:49:51 +0800

    Site updated: 2023-04-13 17:49:50

--- js/nav.js
+++ js/nav.js
@@ -1,36 +1,64 @@
+// document.addEventListener('pjax:complete', tonav);
+// document.addEventListener('DOMContentLoaded', tonav);
+// //响应pjax
+// function tonav(){
+// document.getElementById("name-container").setAttribute("style", "display:none");
+
+// var position = $(window).scrollTop();
+
+// $(window).scroll(function () {
+
+//   var scroll = $(window).scrollTop();
+
+//   if (scroll > position) {
+
+
+//     document.getElementById("name-container").setAttribute("style", "");
+//     document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+
+//   } else {
+
+
+//     document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+//     document.getElementById("name-container").setAttribute("style", "display:none");
+
+//   }
+
+//   position = scroll;
+
+// });
+// function scrollToTop(){
+//     document.getElementsByClassName("menus_items")[1].setAttribute("style","");
+//     document.getElementById("name-container").setAttribute("style","display:none");
+//     btf.scrollToDest(0, 500);
+// }
+// //修复没有弄右键菜单的童鞋无法回顶部的问题
+// document.getElementById("page-name").innerText = document.title.split(" | HackerCG 🍁")[0];}
+
 document.addEventListener('pjax:complete', tonav);
 document.addEventListener('DOMContentLoaded', tonav);
 //响应pjax
-function tonav(){
-document.getElementById("name-container").setAttribute("style", "display:none");
+function tonav() {
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    var position = $(window).scrollTop();
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+            document.getElementById("name-container").setAttribute("style", "");
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+        } else {
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+            document.getElementById("name-container").setAttribute("style", "display:none");
+        }
+        position = scroll;
+    });
+    //修复没有弄右键菜单的童鞋无法回顶部的问题
+    document.getElementById("page-name").innerText = document.title.split(" | HackerCG 🍁")[0];
+}
 
-var position = $(window).scrollTop();
-
-$(window).scroll(function () {
-
-  var scroll = $(window).scrollTop();
-
-  if (scroll > position) {
-
-
-    document.getElementById("name-container").setAttribute("style", "");
-    document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
-
-  } else {
-
-
+function scrollToTop() {
     document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
     document.getElementById("name-container").setAttribute("style", "display:none");
-
-  }
-
-  position = scroll;
-
-});
-function scrollToTop(){
-    document.getElementsByClassName("menus_items")[1].setAttribute("style","");
-    document.getElementById("name-container").setAttribute("style","display:none");
     btf.scrollToDest(0, 500);
 }
-//修复没有弄右键菜单的童鞋无法回顶部的问题
-document.getElementById("page-name").innerText = document.title.split(" | HackerCG 🍁")[0];}
+
